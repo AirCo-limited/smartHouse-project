5699b4634c5d952c5a493ddd81431020bae146fb
maryam <maryam.qsi77@gmail.com>
2021-11-25 10:30:39 +0330

    Frontend

---
+++ src/scripts.js
@@ -0,0 +1,35 @@
+function updateClock() {
+    let now = new Date();
+    let dName = now.getDay(),
+        mo = now.getMonth(),
+        dNum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "AM";
+    if (hou === 0) {
+        hou = 12
+    }
+    if (hou > 12) {
+        hou = hou - 12;
+        pe = "PM"
+    }
+    Number.prototype.pad = function (digits) {
+        for (let n = this.toString(); n.length < digits; n = 0 + n) ;
+        return n;
+    }
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "september", "October", "November", "December"];
+    let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "second", "period"];
+    let values = [week[dName], months[mo], dNum.p, yr, hou, min, sec, pe];
+    for (let i = 0; i < ids.length; i++) {
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+
+    }
+}
+
+function initClock() {
+    updateClock();
+    window.setInterval("updateClock()", 1)
+}
